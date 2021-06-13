64928d1c7bec6972751c336345b78475288699b5
George Alusa <40027452+georgealusa@users.noreply.github.com>
2021-06-13 21:45:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-
+let daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let maleKhanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleKhanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
