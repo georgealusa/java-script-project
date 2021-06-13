da9994355f2208b3510c45ca6b47bf1bb55255d7
George Alusa <40027452+georgealusa@users.noreply.github.com>
2021-06-13 21:45:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let maleKhanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleKhanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let formValidation = function(){
+    hideElement();
+    let dateData = document.forms["khanEntry"]["data"].value
+    let genderData = document.forms["khanEntry"]["gender"].value
+
